b9069e2f46adf8fccdb6ce09b8f9830ff0a09de8
priyanka.thakur.bdm.mumbai <priyanka.thakur.bdm.mumbai@gmail.com>
2024-11-09 12:52:16 +0530

    Added a new project "SimpleClass_01" to the solution "chapter_02"

---
+++ chapter_02/SimpleClass_01/Program.cs
@@ -0,0 +1,38 @@
+﻿// Program to create Simple Class
+// Programmer : Priyanka Thakur
+
+namespace SimpleClass_01
+{
+    // Defining a class named "Car"
+    public class Car
+    {
+        // Fields: Data that the Car holds
+        public string color; // Color of the car
+        public int speed;    // Speed of the car
+
+        // Method: An action that the Car can perform
+        public void Drive()
+        {
+            // This method will print a message when called
+            Console.WriteLine("The car is driving at " + speed + " km/h.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to create Simple Class\n");
+
+            // Create an object of the Car class
+            Car myCar = new Car(); // This is like building a new robot from the blueprint
+
+            // Assign values to the fields of the object
+            myCar.color = "Red";
+            myCar.speed = 100;
+
+            // Call the method to perform the action
+            myCar.Drive(); // Output: The car is driving at 100 km/h.
+        }
+    }
+}
