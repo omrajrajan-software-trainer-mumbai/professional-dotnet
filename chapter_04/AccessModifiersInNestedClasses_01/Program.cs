e69f9b9fef6bfc929ad081c9f3206c5de08c0a8f
Nikhil Patil <121230134+nikhil-patil-programmer-mumbai@users.noreply.github.com>
2024-12-04 12:08:38 +0530

    added a new project "AccessModifiersInNestedClasses_01" in solution "chapter_04"

---
+++ chapter_04/AccessModifiersInNestedClasses_01/Program.cs
@@ -0,0 +1,33 @@
+﻿// Program to demonstrate Access Modifiers In Nested Classes in C# with Visual Studio
+// Programmer: Nikhil Patil
+
+namespace AccessModifiersInNestedClasses_01
+{
+    class OuterClass
+    {
+        private class InnerClass
+        {
+            // Private method within nested class
+            public void InnerClassMethod()
+            {
+                Console.WriteLine("Inner Class Method");
+            }
+        }
+
+        public void AccessInnerClass()
+        {
+            InnerClass innerClass = new InnerClass();
+            innerClass.InnerClassMethod(); // Accessing private nested class method
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to demonstrate Access Modifiers In Nested Classes in C# with Visual Studio\n");
+
+            OuterClass outerClass = new OuterClass();
+            outerClass.AccessInnerClass(); // Output: Inner Class Method
+        }
+    }
+}
