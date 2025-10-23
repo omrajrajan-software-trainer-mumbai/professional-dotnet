821c6c56233c6228b2bf365810913271308f006f
kavya-krishnan-paramasivan-programmer <kkp592006@gmail.com>
2025-10-24 01:11:45 +0530

    Added new projects to Chapter_10

--- Chapter_10/01_SingletonPattern/obj/Debug/net8.0/01_SingletonPattern.AssemblyInfo.cs
+++ Chapter_10/01_SingletonPattern/obj/Debug/net8.0/01_SingletonPattern.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01_SingletonPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2754e332c9c3c299f6b90e66a044bd637ebb94b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4294bc0e591066111952ed7da46efd415b01de1")]
 [assembly: System.Reflection.AssemblyProductAttribute("01_SingletonPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01_SingletonPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Chapter_10/13_MultipleCatchBlocks/Program.cs
@@ -0,0 +1,56 @@
+﻿//Program to demonstrate the use of multiple catch blocks in C# with Visual Studio
+// Programmer: Kavya Krishnan
+namespace _13_MultipleCatchBlocks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to demonstrate the use of multiple catch blocks in C# with Visual Studio\n");
+
+            // Infinite loop to repeatedly take user input until the user decides to exit
+            while (true)
+            {
+                try
+                {
+                    // Prompt the user to enter a number or hit return to exit
+                    Console.Write("Input a number between 0 and 5 or just hit return to exit)> ");
+                    string? userInput = Console.ReadLine();
+
+                    // If the user presses Enter without typing anything, exit the loop
+                    if (string.IsNullOrEmpty(userInput))
+                    {
+                        break;
+                    }
+
+                    // Convert the string input to an integer
+                    int index = Convert.ToInt32(userInput);
+
+                    // If the number is not between 0 and 5, throw a custom IndexOutOfRangeException
+                    if (index < 0 || index > 5)
+                    {
+                        throw new IndexOutOfRangeException($"You typed in {userInput}");
+                    }
+
+                    // Display the valid number entered by the user
+                    Console.WriteLine($"Your number was {index}");
+                }
+                // This block handles IndexOutOfRangeException specifically
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Exception: Number should be between 0 and 5. {ex.Message}");
+                }
+                // This general catch block handles any other type of exception
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An exception was thrown. Exception type: {ex.GetType().Name}, Message: {ex.Message}");
+                }
+                // The finally block always executes, whether an exception occurs or not
+                finally
+                {
+                    Console.WriteLine("Thank you\n");
+                }
+            }
+        }
+    }
+}
